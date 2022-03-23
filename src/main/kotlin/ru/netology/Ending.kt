f578517943684aca999f22ad13d2b50131639571
mstisf <mstisf@gmail.com>
2022-03-23 22:30:26 +0300

    Исправлен баг для количества лайков заканчивающихся на 11

--- src/main/kotlin/ru/netology/Ending.kt
+++ src/main/kotlin/ru/netology/Ending.kt
@@ -1,12 +1,13 @@
 package ru.netology
 
 fun main() {
-    val likes = 0
+    val likes = 111
     println("Понравилось $likes ${getHumanWord(likes)}")
 }
 
 fun getHumanWord(likes: Int): String {
     val lastDigi = likes % 10
-    if (lastDigi == 1) return "человеку"
+    val lastTwoDigi = likes % 100
+    if (lastDigi == 1 && lastTwoDigi != 11) return "человеку"
     return "людям"
 }
